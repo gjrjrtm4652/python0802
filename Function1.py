ec430e0d9bc3595da261448987dfdbe641069dbc
gjrjrtm4652 <88297253+gjrjrtm4652@users.noreply.github.com>
2021-08-03 17:32:15 +0900

    함수연습2

--- Function1.py
+++ Function1.py
@@ -23,3 +23,29 @@ def intersect(prelist, postlist):
     return result
 
 print (intersect('HAM',"SPAM"))
+
+a=1.2
+print(id(a))
+a=2.3
+print(id(a))
+
+print("====가변형식====")
+lst = [1,2,3]
+print(id(lst))
+lst.append(4)
+print(id(lst))
+
+#객체는 참조를 통해 입출력이 된다.
+
+wordlist = ["J","A","M"]
+
+def change(x):
+
+    x1 = x[:]
+    x1[0] = "H"
+    print("함수내부:" ,x1)
+    #x[0] = "H"
+
+    
+change(wordlist)
+print("함수 호출후:",wordlist)
