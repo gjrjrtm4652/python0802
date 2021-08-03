03e98898af2b377d7a9646fcba871914a255651a
gjrjrtm4652 <88297253+gjrjrtm4652@users.noreply.github.com>
2021-08-03 17:59:21 +0900

    함수연습3

--- Function2.py
+++ Function2.py
@@ -12,3 +12,40 @@ def func2(a):
     return x+a
 
 print (func2(1))        
+
+#불변형식 함수 내부에서 읽기+쓰기
+g=1
+def testScope(a):
+    # global g
+    g=2
+    return g+a
+#호출
+print(testScope(1))
+print("전역변수 g:" ,g)    
+
+def times(a=10,b=20):
+    return a*b
+
+print (times())
+print(times(a=5))
+print(times(3,4))
+
+#키워드 인자 (파라메터명을 명시)
+def connectURI(server,port):
+    strURL = "http://" + server + ":" + port
+    return strURL
+
+print(connectURI("bitCamp","80"))
+print(connectURI(port="80",server="bitCamp"))
+
+#가변인자(다양한 갯수를 소화)
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+print (union ("HAM","EGG"))
+print (union ("HAM","EGG","SPAM"))            
