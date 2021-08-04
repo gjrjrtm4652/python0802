a079dfe71950ed65cf39283b4517db3523351759
gjrjrtm4652 <88297253+gjrjrtm4652@users.noreply.github.com>
2021-08-04 11:45:57 +0900

    클래스2

--- class2.py
+++ class2.py
@@ -1,14 +1,12 @@
-# class1.py
+# class2.py
 
 class Person:
     def __init__(self):
         self.name = "default name"
+        Person.num_person += 1
     def print(self):
         print ("My name is {0}".format(self.name))
 
 p1 = Person()
 p2 = Person()
-p1.name = "전우치"
-p1.print()       
-p2.print()     
-        
+print("인스턴스 갯수:{0}".format(Person.num_person))  
