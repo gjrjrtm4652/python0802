64b7e1c275a3a29f6170532fff698f6b049ed83e
gjrjrtm4652 <88297253+gjrjrtm4652@users.noreply.github.com>
2021-08-04 13:00:12 +0900

    클래스3

--- class2.py
+++ class2.py
@@ -1,6 +1,7 @@
 # class2.py
 
 class Person:
+    num_person = 0
     def __init__(self):
         self.name = "default name"
         Person.num_person += 1
@@ -9,4 +10,16 @@ class Person:
 
 p1 = Person()
 p2 = Person()
+p3 = Person()
 print("인스턴스 갯수:{0}".format(Person.num_person))  
+
+#동적으로 확장이 된다
+Person.title = "new title"
+print(p1.title)
+print(p2.title)
+print(Person.title)
+
+#인스턴스에 추가
+p1.age = 30
+print(p1.age)
+# print(p2.age)
