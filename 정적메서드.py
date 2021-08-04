cca69c3816ec3a27c065400c586be08039e6f602
gjrjrtm4652 <88297253+gjrjrtm4652@users.noreply.github.com>
2021-08-04 15:54:13 +0900

    클래스3

--- 정적메서드.py
+++ 정적메서드.py
@@ -1,10 +1,15 @@
 # 정적메서드.py
 class MyCalc(object):
+    #클래스 소속(정적메서드)
     @staticmethod
     def my_add(x,y):
         return x+y
+    def methodA(self):
+        print("인스턴스 메서드")
 
 #클래스에서 직접 호출한다.
 a = MyCalc.my_add(5,7)
 print(a)
-
+#인스턴스 생성
+calc = MyCalc()
+camc.methodA()
