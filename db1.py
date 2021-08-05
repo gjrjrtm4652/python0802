3df206cdeed738e13356c8aeb6ca3034c7ebdfcb
gjrjrtm4652 <88297253+gjrjrtm4652@users.noreply.github.com>
2021-08-05 17:49:23 +0900

    데이터 베이스2

--- db1.py
+++ db1.py
@@ -2,7 +2,8 @@
 import sqlite3
 
 #연결객체 생성(일단은 메모리에 임시로 저장)
-con = sqlite3.connect(":memory:")
+#con = sqlite3.connect(":memory:")
+con = sqlite3.connect("c:\\work\\test.db")
 #구문을 수행할 커서 객체를 생성
 cur = con.cursor()
 #데이터를 담을 테이블 생성
